11fd8d5ca7836e212896742059e9b22a526fac13
DEEPAKGUPTA1141421 <deepak2198.be21@chitkara.edu.in>
2023-10-30 02:17:41 +0530

    commit by vs code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+import './App.css';
+function Ceel({filled,onClick,isDisabled}){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <button
+  type='button'
+  className={filled?"cell-activated":"cell"}
+  onClick={onClick}
+  disabled={isDisabled}
+  />
+  )
+}
+function App() {
+  const [order,setOrder]=useState([]);
+  const [isDeactivting,setIsDeactivating]=useState(false);
+    const config=[
+    [1,1,1],
+    [1,0,1],
+    [1,1,1]
+  ]
+  const deactivatedcell=()=>{
+    setIsDeactivating(true);
+    const timer=setInterval(()=>{
+      setOrder((origOrder)=>{
+        const newOrder=origOrder.slice();
+        newOrder.pop();
+        if(newOrder.length===0){
+          clearInterval(timer);
+          setIsDeactivating(false);
+        }
+        return newOrder;
+      })
+    },300);
+  };
+  const activateCell=(index)=>{
+    // if(order.includes(index)){
+    //   console.log("included");
+    //   return;
+    // }
+    const neworder=[...order,index];
+    setOrder(neworder);
+    console.log(neworder);
+    if(neworder.length===config.flat(1).filter(Boolean).length){
+      deactivatedcell();
+    } 
+      
+  };
+  return (
+    <div className='wrapper'>
+      <div className="grid"
+      style={{gridTemplateColumns:`repeat(${config[0].length},1fr)`}}
+      >
+      {config.flat(1).map((element,index)=>(
+       element?<Ceel 
+       key={index}
+       filled={order.includes(index)}
+       onClick={(e)=>activateCell(index)}
+       isDisabled={order.includes(index)||isDeactivting}
+       />:<span></span>
+      ))}
+        </div>
     </div>
   );
 }
